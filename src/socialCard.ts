587137d7f535c65e94c669dd178497c4199b0ad2
mattcroat <mattcroat.dev@gmail.com>
2021-10-06 20:47:11 +0200

    feat: Image compression

--- src/socialCard.ts
+++ src/socialCard.ts
@@ -1,4 +1,5 @@
 import * as htmlToImage from 'html-to-image'
+import imageCompression from 'browser-image-compression'
 
 const cardElement = document.querySelector<HTMLDivElement>('[data-card]')!
 const saveElement = document.querySelector<HTMLButtonElement>('[data-save]')!
@@ -6,11 +7,16 @@ const saveElement = document.querySelector<HTMLButtonElement>('[data-save]')!
 async function saveImage(node: HTMLElement) {
   try {
     const options = { width: 1200, height: 630 }
-    const dataUrl = await htmlToImage.toPng(node, options)
+    const imageBlob = (await htmlToImage.toBlob(node, options)) as File
+
+    const compressedFile = await imageCompression(imageBlob, {
+      initialQuality: 1,
+    })
+    const imageUrl = URL.createObjectURL(compressedFile)
 
     const linkElement = document.createElement('a')
     linkElement.download = 'og-image.png'
-    linkElement.href = dataUrl
+    linkElement.href = imageUrl
     linkElement.click()
   } catch (error) {
     throw new Error(`Something went wrong: ${error}`)
